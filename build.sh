571639f7c443d01aea37eaf1c28bea15683c4931
BuildTools <beckadamtheartist@gmail.com>
2020-10-06 15:34:54 -0600

    Fix Linux builds

--- build.sh
+++ build.sh
@@ -5,7 +5,8 @@ cp -f bos.inc src/include/bos.inc
 cp -rf src/include src/data/adrive/src/include/
 cp -rf src/data/adrive/src/include src/data/adrive/src/lib/include/
 cd src/data/adrive
-bash build.sh
+chmod +x build.sh
+./build.sh
 cd ../../../
 fasmg src/main.asm bin/BOS.8xp
 python build_docs.py
