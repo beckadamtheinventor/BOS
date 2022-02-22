7790483147766450ff851ab83288fdb9e97446c6
Adam Beckingham <beckadamtheartist@gmail.com>
2022-02-21 19:43:30 -0700

    Remove unused file

--- src/sys/ExecuteFile16bpp.asm
+++
@@ -1,7 +0,0 @@
-
-;@DOES execute a file given a file path, passing arguments via argc/argv, starting it in 16 bit mode.
-;@INPUT int sys_ExecuteFile16bpp(const char *path, int argc, char *argv[]);
-;@DESTROYS All, OP6.
-sys_ExecuteFile16bpp:
-	jq sys_ExecuteFile
-
