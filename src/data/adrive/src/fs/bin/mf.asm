d5c3663eb6dcf080292ef62a11c57391cbe0737a
BeckATI on Win10 <40156145+beckadamtheinventor@users.noreply.github.com>
2022-07-11 14:15:36 -0600

    Rewrite mf.asm since it didn't get added before

---
+++ src/data/adrive/src/fs/bin/mf.asm
@@ -0,0 +1,57 @@
+
+	jr mf_main
+	db "FEX",0
+mf_main:
+	ld hl,.str_usermem_bytes
+	call bos.gui_PrintLine
+	ld hl,bos.end_of_usermem-ti.userMem
+	ld de,(bos.top_of_UserMem)
+	or a,a
+	sbc hl,de
+	push hl
+	ld hl,-ti.userMem
+	add hl,de
+	call bos.gui_PrintUInt
+	ld hl,.str_bytes_used
+	call bos.gui_PrintLine
+	pop hl
+	call bos.gui_PrintUInt
+	ld hl,.str_bytes_free
+	call bos.gui_PrintLine
+
+	ld hl,.str_malloc_bytes
+	call bos.gui_PrintLine
+	ld hl,bos.malloc_cache
+	ld bc,bos.malloc_cache_len
+	ld de,-1
+	xor a,a
+.check_malloc_loop:
+	inc de
+	cpir
+	jr z,.check_malloc_loop
+	ex hl,de
+	ld c,bos.malloc_block_size_bits
+	call ti._ishl
+	push hl
+	ex hl,de
+	ld hl,bos.malloc_cache_len * bos.malloc_block_size
+	or a,a
+	sbc hl,de
+	call bos.gui_PrintUInt
+	ld hl,.str_bytes_used
+	call bos.gui_PrintLine
+	pop hl
+	call bos.gui_PrintUInt
+	ld hl,.str_bytes_free
+	call bos.gui_PrintLine
+	or a,a
+	sbc hl,hl
+	ret
+.str_usermem_bytes:
+	db "Usermem bytes:",0
+.str_malloc_bytes:
+	db "Malloc bytes:",0
+.str_bytes_used:
+	db " bytes used,",0
+.str_bytes_free:
+	db " bytes free,",$A,0
