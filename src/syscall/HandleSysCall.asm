82e68e8df4edf55f1a7410403629334beeb2cd71
Adam Beckingham <beckadamtheartist@gmail.com>
2023-03-23 19:10:45 -0700

    Preserve all registers between syscall invocation and execution so that any register can be passed, at the expense of performance

--- src/syscall/HandleSysCall.asm
+++ src/syscall/HandleSysCall.asm
@@ -1,9 +1,9 @@
 
 sc_HandleSysCall:
-	push hl,ix,iy
+	push hl,af,de,bc,ix,iy
 	ld iy,0
 	add iy,sp
-	ld hl,(iy+9) ; return address
+	ld hl,(iy+18) ; return address
 	xor a,a
 	ld bc,$FFFFFF
 	ld (fsOP6),bc
@@ -11,18 +11,19 @@ sc_HandleSysCall:
 	inc hl
 	inc hl
 	inc hl
-	ld bc,(iy+9)
-	ld (iy+9),hl ; advance return address
+	ld bc,(iy+18)
+	ld (iy+18),hl ; advance return address
 	ld hl,str_SysCallsVar
 	push bc,hl,de
 	call sc_LoadSysCall
 	pop bc,bc,de
-	pop iy,ix
+	pop iy,ix,bc
 	jr c,.fail
+	pop de,af
 	ex (sp),hl
 	ret
 .fail:
-	pop hl
+	pop hl,af,hl
 	push de
 	ld hl,str_UnimplementedSysCall
 	call gui_DrawConsoleWindow
