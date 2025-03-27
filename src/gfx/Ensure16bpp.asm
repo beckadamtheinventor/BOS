2376faa7854f6cbb08ca350e27ff93fed778dbc2
Adam Beckingham <beckadamtheartist@gmail.com>
2025-03-26 20:49:52 -0600

    Fix typo

--- src/gfx/Ensure16bpp.asm
+++ src/gfx/Ensure16bpp.asm
@@ -1,7 +1,7 @@
 ;@DOES Ensure that the lcd to 8bpp mode. If not in 16bpp mode, clears vram.
 ;@DESTROYS HL,DE,BC,AF
 gfx_Ensure16bpp:
-    ld a,(ti.mpLcdUpbase)
+    ld a,(ti.mpLcdCtrl)
     cp a,ti.lcdBpp16
     ret z
     jq gfx_Set16bpp
--- src/gfx/Ensure8bpp.asm
+++ src/gfx/Ensure8bpp.asm
@@ -1,7 +1,9 @@
 ;@DOES Ensure that the lcd to 8bpp mode. If not in 8bpp mode, clears vram.
-;@DESTROYS HL,DE,BC,AF
+;@DESTROYS Assume all.
 gfx_Ensure8bpp:
-    ld a,(ti.mpLcdUpbase)
+    ;ld hl,(cur_lcd_buffer)
+    ;ld (ti.mpLcdUpbase),hl
+    ld a,(ti.mpLcdCtrl)
     cp a,ti.lcdBpp8
     ret z
     jq gfx_Set8bpp
